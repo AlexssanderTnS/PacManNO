01d6454c858568a1d6fc65353e82829c9c199760
EduardoWill <eduardo.william188@gmail.com>
2025-05-29 19:59:04 -0300

    Botão para limpar os campos do Login

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,21 @@
+const  usuarioCadastrado = {
+    email:"email@exemplo.com",
+    senha: dawdawd,
+};
+function limparUsuario(){
+    document.getElementById('usuario').value='';
+};
+
+//Limpar senha
+function limparSenha(){
+    document.getElementById('senha').value=''; 
+};
+//Cadastro
+function cadastro(){
+
+};
+
+
 //Conferir se o login está certo
 function login(){
     document.getElementById('usuario').value;
