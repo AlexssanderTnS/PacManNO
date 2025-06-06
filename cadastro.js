802b8b4749cde24dc1de834ff34ddea1fb975920
EduardoWill <eduardo.william188@gmail.com>
2025-06-06 20:56:00 -0300

    Botão email ficando vermelho

--- cadastro.js
+++ cadastro.js
@@ -2,9 +2,24 @@ const usuarioCadastrado = {
     email: "email@exemplo.com",
     senha: "senhaa",
 };
+const email = document.getElementById("emailCadastro")
+const usuario = document.getElementById("usuarioCadastro")
+const nome = document.getElementById("senhaCadastro")
+const form = document.getElementById("form")
 
+const botao = document.getElementById("pronto")
 const emailPadrao = /^[\w]+(\.[\w]+)?@(gmail|hotmail|outlook|email)\.com$/;
 const senhaPadrao = /^[a-zA-Z]{6,}$/;
+
+//Botão enviar
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    checkEmail();
+
+});
+
+
+
 
 // Limpar usuário
 function limparUsuario() {
@@ -16,4 +31,23 @@ function limparSenha() {
     document.getElementById('senha').value = '';
 }
 
+
+function checkEmail(){
+    const emailValue = email.value
+    if (!emailPadrao.test(emailValue)){
+        entradaErro(email, "Email invalido")
+    }
+
+
+}
+function entradaErro(entrada, mensagem){
+    const formItem = entrada.parentElement
+    const mensagemTexto = formItem.querySelector('p')
+
+    mensagemTexto.innerText = mensagem
+
+    console.log("Mostrando erro para:", entrada.id)
+
+    formItem.className = "botao-campo error"
+}
 
